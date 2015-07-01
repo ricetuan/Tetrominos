090fefeac525bb3dd94cfe9339b7e5cdf524671f
liang-fan <liang.fan@gree.net>
2015-07-02 04:54:05 +0900

    mande game portrait, add search resolution orders

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -43,6 +43,27 @@ bool AppDelegate::applicationDidFinishLaunching() {
 
     // set FPS. the default value is 1.0/60 if you don't call this
     director->setAnimationInterval(1.0 / 60);
+    
+    cocos2d::Size targetSize = glview->getFrameSize();
+    
+    
+    FileUtils::getInstance()->addSearchPath("res");
+    
+    std::vector<std::string> searchResolutionOrder(1);
+    
+    if (targetSize.height < 481.0f) {
+        searchResolutionOrder[0] = "resources-1x";
+    } else if (targetSize.height < 1335.0f) {
+        searchResolutionOrder[0] = "resources-2x";
+    } else if (targetSize.height < 1921.0f) {
+        searchResolutionOrder[0] = "resources-3x";
+    } else {
+        searchResolutionOrder[0] = "resources-4x";
+    }
+    
+    FileUtils::getInstance()->setSearchResolutionsOrder(searchResolutionOrder);
+    
+    
 
     register_all_packages();
 
